0d98efc27d1642d4450ae9dbcf5170bae7a49548
RizkyriaHutabarat <rizkyriahutabarat18@gmail.com>
2023-05-19 17:41:30 +0700

    rizkyria

--- js/controller/get.js
+++ js/controller/get.js
@@ -8,14 +8,14 @@ export function isiTablePresensi(results) {
 function isiRow(value) {
   let content = isiTabel
     .replace("#NAMA#", value.biodata.nama)
-    .replace("#NOHP#", value.biodata.phone_number)
+    .replace("#NOHP#", value.biodata.phone_number ? value.biodata.phone_number : value.phone_number ? value.phone_number : "#NOHP#")
     .replace("#JABATAN#", value.biodata.jabatan)
     .replace("#LOKASI#", value.location)
     .replace("#STATUS#", value.checkin)
-    .replace("#HARIKERJA#", value.biodata.hari_kerja)
-    .replace("#JAMKERJA#", value.biodata.jam_kerja ? value.biodata.jam_kerja[0].durasi : "#JAMKERJA#")
-    .replace("#JAMMASUK#", value.biodata.jam_kerja ? value.biodata.jam_kerja[0].jam_masuk : "#JAMMASUK#")
-    .replace("#JAMKELUAR#", value.biodata.jam_kerja ? value.biodata.jam_kerja[0].jam_kerja : "#JAMKELUAR#")
+    .replace("#HARIKERJA#", value.biodata.hari_kerja ? value.biodata.hari_kerja : "HARIKERJA")
+    .replace("#JAMKERJA#", value.biodata.hari_kerja ? value.biodata.jam_kerja[0].durasi : "#JAMKERJA#")
+    .replace("#JAMMASUK#", value.biodata.hari_kerja ? value.biodata.jam_kerja[0].jam_masuk : "#JAMMASUK#")
+    .replace("#JAMKELUAR#", value.biodata.hari_kerja ? value.biodata.jam_kerja[0].jam_keluar : "#JAMKELUAR#")
     .replace("#WARNA#", getRandomColor())
     .replace(/#WARNALOGO#/g, getRandomColorName());
   addInner("iniTabel", content);
