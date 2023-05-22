c424b5338bf79185e69f9b76752c7d844c6b3dff
RizkyriaHutabarat <rizkyriahutabarat18@gmail.com>
2023-05-22 15:02:31 +0700

    rizkyria_week10

---
+++ js/config/url_get_detail.js
@@ -0,0 +1,5 @@
+// Mendapatkan parameter dari URL
+const urlParams = new URLSearchParams(window.location.search);
+const presensiId = urlParams.get("presensiId");
+
+export let urlFetch = "https://hutabarat.herokuapp.com/presensi/" + presensiId;
